9e998544d620191a34c3c85f6f382ba704da6bf2
ManalReyad99 <manalreyad@outlet.com>
2022-07-26 17:14:11 +0200

    Add project files.

---
+++ Ecommerce/Models/ProductColor.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Ecommerce.Models
+{
+    public class ProductColor
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+
+        [ForeignKey("Product")]
+        public int productId { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
---
+++ Ecommerce/Models/Role.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Models
+{
+    public static class Role
+    {
+        public static async Task seedRoleDB(IApplicationBuilder applicationBuilder)
+        {
+            using (var servicesSocet = applicationBuilder.ApplicationServices.CreateScope())
+            {
+                var roleManger = servicesSocet.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+                if (!await roleManger.RoleExistsAsync("Admin"))
+                {
+                    await roleManger.CreateAsync(new IdentityRole("Admin"));
+                }
+                if (!await roleManger.RoleExistsAsync("user"))
+                {
+                    await roleManger.CreateAsync(new IdentityRole("user"));
+                }
+            }
+        }
+
+    }
+}
